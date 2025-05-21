e92cc99859fcda0826dc6164777c0f24a5a4dc17
Vassilijmo <Vassilij.mo@gmail.com>
2025-05-21 17:32:48 +0200

    Weitere Aufgaben

--- src/Datatypes_Variables/DataTypes.java
+++ src/Datatypes_Variables/DataTypes.java
@@ -4,5 +4,25 @@ public class DataTypes {
 
     public static void runDataTypes(){
 
+        String myInitials = "V.M.";
+        int germanyPopulation = 83280000;
+        long earthPopulation = 8062000000L;
+        boolean isDaytime = true;
+        float goalStrikeQuote = 0.65f;
+        byte javaProgramWeeks = 14;
+        double pi = 3.141592654;
+
+        System.out.println();
+        System.out.printf("%-30s %-30s %-30s\n", "Name", "Value", "Datatype");
+        System.out.println();
+        System.out.printf("%-30s %-30s %-30s\n", "Initialen", myInitials, "String");
+        System.out.printf("%-30s %-30d %-30s\n", "Population in Deutschland", germanyPopulation, "int");
+        System.out.printf("%-30s %-30d %-30s\n", "Population auf der Erde", earthPopulation, "long");
+        System.out.printf("%-30s %-30b %-30s\n", "Tageszeit", isDaytime, "boolean");
+        System.out.printf("%-30s %-30.2f %-30s\n", "Torquote", goalStrikeQuote, "float");
+        System.out.printf("%-30s %-30d %-30s\n", "Dauer Java_Programms", javaProgramWeeks, "byte");
+        System.out.printf("%-30s %-30.15f %-30s\n", "Pi", pi, "double");
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
+import Datatypes_Variables.DataTypes;
 import Input_Output.FormattingTextOutput;
 import Input_Output.Geometry;
 import Input_Output.TextInputOutput;
+import Operators.Cylinder;
+import Operators.OneLineOfCode;
+import Operators.TimeUnits;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Input & Output
+        ///Input & Output
         //Aufgabe 1
     //TextInputOutput.runTextInput();
         //Aufgabe 2
         //FormattingTextOutput.printAscii();
         //Aufgabe 3
-        Geometry.runGeometry();
+        //Geometry.runGeometry();
+        /// Datatypes & Variables
+        // Aufgabe 1
+        //DataTypes.runDataTypes();
+        //Aufgabe 2
+
+        /// Operators
+        //1. Time units
+        //TimeUnits.runTimeUnits();
+        //2. Cylinder
+        //Cylinder.runCylinder();
+        //3. One line of code
+        OneLineOfCode.runOneLineOfCode();
     }
 }
